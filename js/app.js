74fd9e34a5cbc47c2b832d2262e21a9414cd278c
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-05-01 16:29:12 +0100

    first deployment to github

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//jshint esversion:6
+
+// Initialize and add the map
+function initMap(){
+
+ // The location of Ibokun
+ const loc = {lat: 7.7861, lng: 4.7226};
+
+ //The map, centered at Ibokun
+ const map = new google.maps.Map(document.querySelector(".map"), {
+  zoom: 14,
+  center: loc
+ });
+
+ //The marker, positioned at Ibokun
+ const marker = new google.maps.Marker({position: loc, map: map});
+}
+
+
+//Sticky menu background
+window.addEventListener('scroll', function(){
+ if(window.scrollY > 150){
+  document.querySelector("#navbar").style.opacity = 0.9;
+ }else{
+  document.querySelector("#navbar").style.opacity = 1;
+ }
+});
+
+//Smooth Scrolling
+$('#navbar a, .btn').on('click', function(ev){
+ if(this.hash !== '') {
+  console.log(this.hash);
+
+  event.preventDefault();
+
+  const hash = this.hash;
+
+  $('html, body').animate({
+   scrollTop: $(hash).offset().top - 100
+  }, 
+  1200
+  );
+ }
+});
